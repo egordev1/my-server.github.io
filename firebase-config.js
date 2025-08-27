4698228ada2c3e34c7b195b034bd06d1af3a2a49
egordev1 <egorlifencev83@gmail.com>
2025-08-27 23:58:40 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,13 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyBvnXv_Yyb8pBhQMJhRPesbTnwAIwcyhms",
-  authDomain: "project-35653.firebaseapp.com",
-  projectId: "project-35653",
-  storageBucket: "project-35653.firebasestorage.app",
-  messagingSenderId: "760194422661",
-  appId: "1:760194422661:web:64766de4ff774d58ba56ec",
-  measurementId: "G-HT716RM159"
+    apiKey: "AIzaSyBvNXv_Yyb8pBhQMhRPesbTnwAIwcyhms",
+    authDomain: "project-35653.firebaseapp.com",
+    projectId: "project-35653",
+    storageBucket: "project-35653.appspot.com",
+    messagingSenderId: "760194422661",
+    appId: "1:760194422661:web:64766de4ff774d58ba56ec",
+    measurementId: "G-HT716RM159"
 };
+
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
